8a60e530f5ceb373c926e303c29cc77c9d7bd33d
jorgeblanc9 <jorgeblanc9@gail.com>
2025-08-25 19:57:54 -0400

    Comentar el componente HelloWorld y mantener el encabezado "Welcome to Your Vue.js App" en App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
+      <h1>Welcome to Your Vue.js App</h1>
     </div>
   </header>
 
