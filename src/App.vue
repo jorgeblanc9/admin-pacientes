df7e0a1c99445ca2abdddc757fe501bab7ce8254
jorgeblanc9 <jorgeblanc9@gail.com>
2025-08-25 20:21:40 -0400

    Comentar el componente HelloWorld y mantener el encabezado "Hola Ronald" en App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,9 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
+
+      <h1>Hola Ronald</h1>
     </div>
   </header>
 
