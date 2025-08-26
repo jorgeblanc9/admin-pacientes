276cee60562d089f7247c1c5f858037e037f5630
jorgeblanc9 <jorgeblanc9@gail.com>
2025-08-25 22:05:22 -0400

    Agregar el reporter de SonarQube para Vitest y configurar la cobertura de pruebas, estableciendo umbrales mínimos y formatos de salida para los reportes.

--- vitest.config.js
+++ vitest.config.js
@@ -9,6 +9,33 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      globals: true,
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html', 'lcov'],
+        exclude: [
+          'node_modules/',
+          'dist/',
+          '**/*.d.ts',
+          '**/*.config.js',
+          '**/*.config.ts',
+          'coverage/**',
+          'tests/**',
+          'script/**'
+        ],
+        thresholds: {
+          global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80
+          }
+        }
+      },
+      reporters: ['default', 'vitest-sonar-reporter'],
+      outputFile: {
+        'vitest-sonar-reporter': 'sonar-report.xml'
+      }
     },
   }),
 )
